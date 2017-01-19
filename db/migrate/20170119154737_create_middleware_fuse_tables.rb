9e67f3d449d48f4d83122f043a79640ec1516c3c
Jirka Kremser <jkremser@redhat.com>
2017-01-19 17:36:25 +0100

    Middleware Provider + FUSE PoC: Migrations

---
+++ db/migrate/20170119154737_create_middleware_fuse_tables.rb
@@ -0,0 +1,35 @@
+class CreateMiddlewareFuseTables < ActiveRecord::Migration[5.0]
+  def change
+    create_table :middleware_fuse_servers do |t|
+      t.string :name
+      t.string :ems_ref
+      t.string :nativeid
+      t.string :feed
+      t.text   :properties
+      t.bigint :ems_id
+      t.timestamps :null => false
+    end
+
+    create_table :middleware_camel_contexts do |t|
+      t.string :name
+      t.string :ems_ref
+      t.string :nativeid
+      t.string :feed
+      t.bigint :fuse_server_id
+      t.bigint :ems_id
+      t.timestamps :null => false
+    end
+
+    create_table :middleware_camel_entities do |t|
+      t.string :name
+      t.string :ems_ref
+      t.string :nativeid
+      t.string :feed
+      t.string :entity_type
+      t.bigint :camel_context_id
+      t.text   :properties
+      t.bigint :ems_id
+      t.timestamps :null => false
+    end
+  end
+end
